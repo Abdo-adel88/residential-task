1077a0db4289930e525f26504ab45d73440694bc
Abdo-adel88 <abdoade73@gmail.com>
2024-09-04 20:59:26 +0300

    change css

--- src/app/component/house/house.component.ts
+++ src/app/component/house/house.component.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
 })
 export class HouseComponent {
   selectedCard: string = '';
-  
+
+
+  selectCard(card: string) {
+    this.selectedCard = card;
+  }
 }
